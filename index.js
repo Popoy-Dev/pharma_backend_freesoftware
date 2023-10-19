b8b314852d02b8c8a728046131fa1f0763c79d78
Popoy-Dev <popoykua28@gmail.com>
2023-10-19 20:09:56 +0800

    fix printer

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const Printer = require('node-thermal-printer');
 const Order = require('./models/orders')
 
-app.use(cors())
+
 const app = express()
 const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser');
@@ -14,7 +14,9 @@ const electron = typeof process !== 'undefined' && process.versions && !!process
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors({
+    origin: '*', // Allow requests from any origin (for development/testing only)
+}));
 const printer = new Printer.ThermalPrinter({
     type: Types.EPSON, // Printer type: 'star' or 'epson'
     interface: 'printer:XP-58 (copy 3)', // Printer
@@ -36,45 +38,48 @@ const connectDB = async () => {
 
 
 app.post('/add', async (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+
     const { cartList, customerMoney, total } = req.body;
+
+    const product = new Order({
+        cartList,
+      });
+      
+      product.save().then(() => {
+        console.log('Product saved successfully!');
+      }).catch((err) => {
+        console.log(err);
+      });
 
     const isConnected = await printer.isPrinterConnected();
     if (isConnected) {
-      console.log('isConnected', isConnected)
-      printer.openCashDrawer();
-      printer.clear();
+        console.log('isConnected', isConnected)
+        printer.openCashDrawer();
+        printer.clear();
 
-      cartList.map(async (item) => {
-        console.log(item);
-        printer.bold(false);
-        printer.alignLeft();
-        printer.print(String(`${item.product_name} (${item.quantity} x ${item.selling_price}) `));
-        printer.bold(true);
-        printer.print(String(`${item.quantity * item.selling_price}`));
+        cartList.map(async (item) => {
+            printer.bold(false);
+            printer.alignLeft();
+            printer.print(String(`${item.product_name} (${item.quantity} x ${item.selling_price}) `));
+            printer.bold(true);
+            printer.print(String(`${item.quantity * item.selling_price}`));
+            printer.newLine();
+        });
+        printer.alignRight();
+        printer.print(String(`Customer Money: ${customerMoney}`));
         printer.newLine();
-      });
-      printer.alignRight();
-      printer.print(String(`Customer Money: ${customerMoney}`));
-      printer.newLine();
-      printer.print(String(`Total: ${total}`));
-      printer.newLine();
-      await printer.execute();
-      res.status(201).json({ msg: "Print successfully" });
+        printer.print(String(`Total: ${total}`));
+        printer.newLine();
+        await printer.execute();
+        res.status(201).json({ msg: "Print successfully" });
     } else {
-      res.status(500).json({ msg: "printer is not connected" })
+        res.status(500).json({ msg: "printer is not connected" })
     }
 
-    try {
-        await Order.insertMany(cartList)
-    } catch (error) {
-        console.log(error)
-    }
 })
+
 app.get('/allOrder', async (req, res) => {
+
     const orders = await Order.find()
     if (orders) {
         res.json(orders)
--- models/orders.js
+++ models/orders.js
@@ -2,7 +2,21 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
-    orders: { type: [String], required: true }
+    cartList: [
+        {
+          id: String,
+          product_name: String,
+          category: String,
+          indication: String,
+          manufacture_price: Number,
+          selling_price: Number,
+          isVat: String,
+          stockTotal: Number,
+          key: String,
+          quantity: Number,
+          senior_selling_price: String,
+        },
+      ],
 })
 
 module.exports = mongoose.model('Orders', OrderSchema)
